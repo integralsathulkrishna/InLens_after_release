c5383bfd29c6c77ca25e53eaef0c6e36a49e2940
AthulKrishna14310 <athul.krishna14310@gmail.com>
2019-01-02 13:24:54 +0530

    Recent Image Push final Athul

--- app/src/main/java/integrals/inlens/Services/RecentImageService.java
+++ app/src/main/java/integrals/inlens/Services/RecentImageService.java
@@ -35,7 +35,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.OnProgressListener;
+
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
